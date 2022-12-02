0d3eea7879d70ffd30c1b1001b0919609f14a5b1
agaqmk <torvalds@users.noreply.github.com>
2022-12-01 21:01:05 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Powered by <a href="https://nexuspipe.com/?utm_source=ksfrontend" target="_blank" style="color:#ffa820" rel="noreferrer">NexusPIPE</a></p>
+	</footer>
+</div>
+
+<style lang="scss">
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 16px 16px;
+		position: fixed; bottom: 8px; left: 50vw; z-index: 5; transform: translate(-50%,0%);
+		background-color: #00000077;
+    border-radius: 8px;
+		backdrop-filter: blur(16px);
+		p {
+		margin: 0 0;
+		}
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import "./styles.css";
+  if (typeof window !== 'undefined') {
+  // @ts-ignore
+  window.hcaptcha_submit = (key:string)=>document.location.href=`/submit1/${key};`
+  let int:any; int = setInterval(()=>{
+    if (document.querySelector('#hcaptcha')){
+      clearInterval(int)
+      // @ts-ignore
+      hcaptcha.render('hcaptcha', {
+        'theme': 'dark',
+        'sitekey': 'yourSiteKey',
+        'callback': 'hcaptcha_submit'
+      })
+    }
+  },50)
+}
+</script>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="🥪">
+  <main class="🍞">
+		<h1>Welcome!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dignissimos quo, illum, repellat error dolores quis in eius atque deserunt quae. Pariatur nobis laborum consequuntur numquam labore distinctio error vero.</p>
+    <form id="hcaptcha">
+    </form>
+    <p>
+      <a href="mailto:example@example.com">Contact us!</a>
+    </p>
+  </main>
+</div>
+<style lang="scss">
+  .🥪 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    min-height: calc(100vh - 14px);
+    .🍞 {
+			h1 {
+        font-size: 1.4em;
+        font-weight: 600;
+			}
+			padding: 32px 32px;
+      background-color: #00000077;
+      border-radius: 8px;
+			backdrop-filter: blur(16px);
+      text-align: center;
+      max-width: 90vw;
+    }
+  }
+</style>
